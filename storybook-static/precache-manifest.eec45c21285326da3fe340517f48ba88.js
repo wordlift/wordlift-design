6d847a9b91df7506fbccf431b0123198620e5fb5
David Riccitelli <david@wordlift.io>
2020-04-09 18:55:57 +0200

    fix tailwindcss not being found.

---
+++ storybook-static/precache-manifest.eec45c21285326da3fe340517f48ba88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52052359a31170cb3622e67f2f44e5a6",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.5b6301c8fc7b8b53985c.bundle.js"
+  },
+  {
+    "url": "runtime~main.5b6301c8fc7b8b53985c.bundle.js"
+  },
+  {
+    "revision": "ff7f8a6a1799e517c088",
+    "url": "static/css/main.68af6b03.chunk.css"
+  },
+  {
+    "revision": "5bfb8ae85443aecb02e0",
+    "url": "static/css/vendors~main.99a6995d.chunk.css"
+  },
+  {
+    "url": "vendors~main.5b6301c8fc7b8b53985c.bundle.js"
+  },
+  {
+    "url": "vendors~main.5b6301c8fc7b8b53985c.bundle.js.LICENSE.txt"
+  }
+]);
