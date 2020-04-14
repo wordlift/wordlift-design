de3242f6b2b5a08a26ff8bccfffe0a42ab0b4b84
David Riccitelli <david@wordlift.io>
2020-04-14 14:57:05 +0200

    cleanup popover stories; add button with onClick action.

--- src/Popover.stories.js
+++ src/Popover.stories.js
@@ -27,54 +27,6 @@ export const allPopovers = () => (
   </div>
 );
 
-const onSelect = () => {
-  console.log("EVENT RECEIVED");
-  const selection = document.getSelection();
-
-  openPopover(selection);
-};
-
-const openPopover = (selection) => {
-  if (0 === selection.rangeCount) return;
-  const range = selection.getRangeAt(0);
-  const rect = range.getBoundingClientRect();
-  const popoverEl = document.createElement("div");
-  const id = "popover-div-" + Math.floor(Math.random() * Math.floor(999999));
-  popoverEl.id = id;
-  popoverEl.style.position = "absolute";
-  popoverEl.style.top = rect.y + "px";
-  popoverEl.style.left = rect.x + rect.width + "px";
-  document.body.appendChild(popoverEl);
-
-  window.postMessage({ type: "wordlift/popover/close", payload: id }, "*");
-
-  ReactDOM.render(<Popover direction={"left"}>Left</Popover>, popoverEl, () => {
-    const listener = (message) => {
-      if (
-        "wordlift/popover/close" === message.data.type &&
-        id !== message.data.payload
-      ) {
-        console.log("MESSAGE RECEIVED");
-        ReactDOM.unmountComponentAtNode(popoverEl);
-        window.removeEventListener("message", listener);
-      }
-    };
-    window.addEventListener("message", listener);
-  });
-};
-
-export const popoverOnSelection = () => (
-  <div style={{ display: "flex" }}>
-    <div contentEditable={true} onSelect={onSelect}>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium
-      aliquam aliquid, aperiam assumenda aut consectetur ducimus eos est
-      excepturi explicabo facilis fuga impedit iste reprehenderit sequi sint,
-      soluta, totam vitae!
-    </div>
-    <div style={{ width: "400px" }} />
-  </div>
-);
-
 export const createPopoverStory = () =>
   createPopover(<Popover>Hello World!</Popover>, {
     top: 200,
@@ -119,12 +71,15 @@ selectionPopover.story = {
             if (range.collapsed) return;
 
             const { top, right, bottom, left } = range.getBoundingClientRect();
-            createPopover(<>Hello Mars!</>, {
-              top,
-              right,
-              bottom,
-              left,
-            });
+            createPopover(
+              <Button onClick={() => alert("Ouch!")}>Hello Mars!</Button>,
+              {
+                top,
+                right,
+                bottom,
+                left,
+              }
+            );
           }}
         >
           <p>
