7f3bafeb1008969a8913fa1a8b31ea747787213a
David Riccitelli <david@wordlift.io>
2020-06-30 10:10:48 +0200

    add initial Carousel

---
+++ src/Carousel.js
@@ -0,0 +1,8 @@
+/**
+ * External dependencies
+ */
+import React from "react";
+
+export default function Carousel() {
+  return <div>Hello World! from Carousel</div>;
+}
---
+++ src/Carousel.stories.js
@@ -0,0 +1,16 @@
+/**
+ * External dependencies
+ */
+import React from "react";
+
+/**
+ * Internal dependencies
+ */
+import Carousel from "./Carousel";
+
+export default {
+  title: "Design System|Carousel",
+  component: Carousel,
+};
+
+export const carousel = () => <Carousel />;
